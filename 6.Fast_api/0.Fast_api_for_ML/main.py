778229f1bb07f1d1e287b6775c217ca073d83bdb
sramirezh <sramirez.hinestrosa@gmail.com>
2023-01-30 19:14:48 -0500

    Started with Fast API

--- 6.Fast_api/0.Fast_api_for_ML/main.py
+++ 6.Fast_api/0.Fast_api_for_ML/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,3 +7,19 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    tags: list[str] = []
+
+
+@app.post("/items/")
+def create_item(item: Item):
+    return item
